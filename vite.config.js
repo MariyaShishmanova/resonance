e98484fe339a220090bb549b0b58fa1f72e08082
Maria Shishmanova <shishmanova.mariya@gmail.com>
2024-02-24 16:39:02 +0200

    new branch created

---
+++ vite.config.js
@@ -0,0 +1,9 @@
+// vite.config.js
+import { defineConfig } from 'vite';
+import VitePluginSass from 'vite-plugin-sass';
+
+export default defineConfig({
+  plugins: [
+    VitePluginSass()
+  ]
+});
